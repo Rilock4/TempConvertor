7928c3aa1cd6d5b3b70eae932c93187ec3e6bc5b
Stephen DeRosa <stephen_derosa@outlook.com>
2023-07-29 16:49:48 -0400

    Update Temperature.java
    Added a while loop and if else statements to check if user in put was a numerical value. If it wasn't a numerical value the user will be prompted to enter one.

--- Temperature Converter/src/tempPack/Temperature.java
+++ Temperature Converter/src/tempPack/Temperature.java
@@ -5,9 +5,24 @@ import javax.swing.JOptionPane;
 public class Temperature {
 
 	public static void main(String[] args) {
-		String temp = JOptionPane.showInputDialog("What is the Temperature?");
-		float c = (float) ((Float.parseFloat(temp) - 32) / 1.8);
-		JOptionPane.showMessageDialog(null, "The temperature in celsius is " +c);
+		
+		while (true) {
+			// prompting user for the temperature
+			String temp = JOptionPane.showInputDialog("What is the temperature?");
+			// checking to see if user input is a number
+			if (!temp.matches("[0-9]+")) {
+				// if not a number than prompt user for a number
+				JOptionPane.showMessageDialog(null, temp+" is not a valid temperature. Please enter a temperature.");			
+			} // if closing look
+			else {
+				float c = (float) ((Float.parseFloat(temp) - 32) / 1.8);
+				JOptionPane.showMessageDialog(null, "The temperature in celsius is " +c);
+				break;
+		    } // else closing brace
+
+		} // while closing brace
+		
 	} // main closing brace
 
 } // Temperature class closing brace
+
